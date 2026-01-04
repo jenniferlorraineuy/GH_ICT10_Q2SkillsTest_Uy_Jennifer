474fec037a819256ccbb49fad3b91798f10eda28
jenniferlorraineuy <uy.js@obmontessori.edu.ph>
2026-01-04 20:31:07 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,86 @@
+from pyscript import display
+
+def club (e):
+    club = {
+        'Name':'Dance Club',
+        'Description':'Making art through your moves',
+        'Meeting time': 'Every Monday and Thursday at 3:00-5:00PM',
+        'Location':'Room 501',
+        'Club Moderator':'Ms. Hopper',
+        'Number of Members':'15'
+    }
+
+    display(f'Name: {club["Name"]}', target='output')
+    display(f'Description: {club["Description"]}', target='output')
+    display(f'Meeting time: {club["Meeting time"]}', target='output')
+    display(f'Location: {club["Location"]}', target='output')
+    display(f'Club Moderator: {club["Club Moderator"]}', target='output')
+    display(f'Number of Members: {club["Number of Members"]}', target='output')
+
+def club1 (e):
+    club = {
+        'Name':'Volleyball Club',
+        'Description':'Throwing balls over nets',
+        'Meeting time': 'Every Friday at 3:00-5:00PM',
+        'Location':'Court',
+        'Club Moderator':'Ms. Wheeler',
+        'Number of Members':'20'
+    }
+
+    display(f'Name: {club["Name"]}', target='output1')
+    display(f'Description: {club["Description"]}', target='output1')
+    display(f'Meeting time: {club["Meeting time"]}', target='output1')
+    display(f'Location: {club["Location"]}', target='output1')
+    display(f'Club Moderator: {club["Club Moderator"]}', target='output1')
+    display(f'Number of Members: {club["Number of Members"]}', target='output1')
+
+def club2 (e):
+    club = {
+        'Name':'Soccer Club',
+        'Description':'Kicking balls to nets',
+        'Meeting time': 'Every Thursday at 3:00-5:00PM',
+        'Location':'Field',
+        'Club Moderator':'Mr. Harrington',
+        'Number of Members':'15'
+    }
+
+    display(f'Name: {club["Name"]}', target='output2')
+    display(f'Description: {club["Description"]}', target='output2')
+    display(f'Meeting time: {club["Meeting time"]}', target='output2')
+    display(f'Location: {club["Location"]}', target='output2')
+    display(f'Club Moderator: {club["Club Moderator"]}', target='output2')
+    display(f'Number of Members: {club["Number of Members"]}', target='output2')
+
+def club3 (e):
+    club = {
+        'Name':'Pep Squad',
+        'Description':'Cheering for wins',
+        'Meeting time': 'Every Wednesday at 3:00-5:00PM',
+        'Location':'Court',
+        'Club Moderator':'Ms. Mayfield',
+        'Number of Members':'20'
+    }
+
+    display(f'Name: {club["Name"]}', target='output3')
+    display(f'Description: {club["Description"]}', target='output3')
+    display(f'Meeting time: {club["Meeting time"]}', target='output3')
+    display(f'Location: {club["Location"]}', target='output3')
+    display(f'Club Moderator: {club["Club Moderator"]}', target='output3')
+    display(f'Number of Members: {club["Number of Members"]}', target='output3')
+
+def club4 (e):
+    club = {
+        'Name':'Baking Club',
+        'Description':'Art through sweet treats',
+        'Meeting time': 'Every Tuesday at 3:00-5:00PM',
+        'Location':'Baking room',
+        'Club Moderator':'Ms. Buckley',
+        'Number of Members':'20'
+    }
+
+    display(f'Name: {club["Name"]}', target='output4')
+    display(f'Description: {club["Description"]}', target='output4')
+    display(f'Meeting time: {club["Meeting time"]}', target='output4')
+    display(f'Location: {club["Location"]}', target='output4')
+    display(f'Club Moderator: {club["Club Moderator"]}', target='output4')
+    display(f'Number of Members: {club["Number of Members"]}', target='output4')
